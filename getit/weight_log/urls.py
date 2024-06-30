966ebd209062bf9f1ce754c949ff2a6aa14e2b4f
Mohammad Khawar <mohammadkhawar@Mohammads-MacBook-Pro-2.local>
2024-06-29 22:47:15 -0700

    delete all records working

--- getit/weight_log/urls.py
+++ getit/weight_log/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.index, name="index"),
+        path("", views.index, name="index"),     
+        path("delete-all/", views.delete_all_records, name="delete_all_records"), 
         ]
--- getit/weight_log/views.py
+++ getit/weight_log/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import WeightLogModel
 from .forms import WeightLogForm
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 
 def index(request):
     form = WeightLogForm(request.POST or None)
-    
+    weight_logs = WeightLogModel.objects.all()
+
     if form.is_valid():
         form.save()
     
-    context = {'form': form}
+    context = {'form': form, 
+               'weight_logs': weight_logs,
+               }
 
-    return render(request, "weight_log/index.html", context)
+    if request.method == "POST":
+        return redirect(reverse('index'))
+    else:
+        return render(request, "weight_log/index.html", context)
+
+
+def delete_all_records(request):
+    WeightLogModel.objects.all().delete()
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
